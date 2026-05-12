5b31110286afe50c92b64e8ebfe50b42dc3ccedc
lucane <contact@lucane.tech>
2026-05-12 02:22:29 +0100

    var deploy

--- contact_filter/settings.py
+++ contact_filter/settings.py
@@ -128,12 +128,12 @@ elif _s3_endpoint:
     }
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
-    AWS_S3_ENDPOINT_URL = _s3_endpoint
-    AWS_S3_REGION_NAME = os.getenv('AWS_REGION', 'auto')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_SBUCKET_NAME')
+    AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'auto')
     AWS_DEFAULT_ACL = 'private'
     AWS_S3_FILE_OVERWRITE = False
-    MEDIA_URL = f"{_s3_endpoint}/{os.getenv('AWS_BUCKET_NAME')}/"
+    MEDIA_URL = f"{os.geten('AWS_S3_ENDPOINT_URL')}/{os.getenv('AWS_STORAGE_BUCKET_NAME')}/"
 else:
     # Production sans bucket (fallback filesystem — déconseillé sur Railway)
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
